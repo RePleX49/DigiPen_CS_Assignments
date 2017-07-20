d11e44a0a95e5923f2a624792e56ef40c58b1c61
RePleX49 <lukelin2009@hotmail.com>
2017-07-20 12:15:00 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,55 +1,34 @@
 #include <iostream>
-#include <iomanip>
 
 int main(void)
 {
-    int userInput = 0;
-    float ValueInput = 0.0f;
-    float ValueOutput = 0.0f;
-    std::cout << std::setprecision(2);
-    std::cout << std::fixed;
-    std::cout << "Choose out of the following options:" << std::endl;
-    std::cout << "1 - Convert from Fahrenheit to Celsius" << std::endl;
-    std::cout << "2 - Convert from Celsius to Fahrenheit" << std::endl;
-    std::cout << "3 - Convert from Miles to Kilometers" << std::endl;
-    std::cout << "4 - Convert from Kilometers to Miles" << std::endl;
-    std::cout << "Press 1, 2, 3 or 4 to choose: ";
-    std::cin >> userInput;
+    int playerOption = 0;
+    int playerGuess = 0;
+    std::cout << "Which game would you like to play?" << std::endl;
+    std::cout << "1 - Human" << std::endl;
+    std::cout << "2 - COmputer" << std::endl;
+    std::cout << "Anything else to Exit!" << std::endl;
+    std::cout << std::endl;
 
-    switch (userInput)
+    std::cout << "Your Choice is: ";
+    std::cin >> playerOption;
+    std::cout << std::endl;
+
+    switch (playerOption)
     {
+    // Human guess
     case 1:
-        std::cout << "Enter the temperature value in Fahrenheit: ";
-        std::cin >> ValueInput;
-        ValueOutput = (ValueInput - 32) * 5 / 9;
-        std::cout << ValueInput << " Fahrenheit = "; 
-        std::cout << ValueOutput << " Celsius" << std::endl;
+        std::cout << "Guess the number I'm thinking of (between 1 and 100, 0=quit): ";
+        std::cin >> playerGuess;
         break;
 
+    // Computer guess
     case 2:
-        std::cout << "Enter the temperature value in Celsius: ";
-        std::cin >> ValueInput;
-        ValueOutput = ValueInput * 9 / 5 + 32;
-        std::cout << ValueInput << " Celsius = ";
-        std::cout << ValueOutput << " Fahrenheit" << std::endl;
-        break;
 
-    case 3:
-        std::cout << "Enter the distance value in Miles: ";
-        std::cin >> ValueInput;
-        ValueOutput = ValueInput * 1.609347F;
-        std::cout << ValueInput << " Miles = ";
-        std::cout << ValueOutput << " Kilometers" << std::endl;
         break;
-
-    case 4:
-        std::cout << "Enter the distance value in Kilometers: ";
-        std::cin >> ValueInput;
-        ValueOutput = ValueInput / 1.609347F;
-        std::cout << ValueInput << " Kilometers = ";
-        std::cout << ValueOutput << " Miles" << std::endl;
-        break;
+    default:
+        return 0;
     }
-    system("pause");
+   
     return 0;
 }
